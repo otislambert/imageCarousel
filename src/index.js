564388a24bd97dcd48b279eed2ef655aad1eb1e7
otis <otislambert@gmail.com>
2022-03-16 11:34:33 -0700

    set timer function

--- src/index.js
+++ src/index.js
@@ -208,6 +208,14 @@ function nextImage(x) {
 }
 
 
-setInterval(function () {
-  nextImage(1);
-}, 5000);
+function timer() {
+  setInterval(function() {
+    console.timeEnd('next');
+    console.time('next');
+    nextImage(1);
+  }, 5000);
+}
+
+window.onload = () => {
+  timer();
+}
